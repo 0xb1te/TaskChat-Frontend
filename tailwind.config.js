78ef72a168cdef2da3039fea9f8dfa2bcacaaa9c
0xb1te <0xb1te0xb2te@gmail.com>
2024-09-09 19:47:14 +0200

    :feat: - Added spartan library

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
+  content: [
+    join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
