536ab16082e70a400e225be477c284111b111259
0xb1te <0xb1te0xb2te@gmail.com>
2024-09-11 21:46:55 +0200

    :feat: - Updated navbar for chat

---
+++ src/app/features/messages/components/message-navbar/message-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-navbar.component.html',
+  styleUrl: './message-navbar.component.scss'
+})
+export class MessageNavbarComponent {
+
+}
