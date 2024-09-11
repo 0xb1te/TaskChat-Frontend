82ac6e5c3cf290c4c246c84258723b2ac3938069
0xb1te <0xb1te0xb2te@gmail.com>
2024-09-11 21:47:58 +0200

    :feat: - Updated navbar style

--- src/app/shared/components/side-navbar/side-navbar.component.ts
+++ src/app/shared/components/side-navbar/side-navbar.component.ts
@@ -1,14 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
+export type Category = {
+  name: string;
+  totalMessages: number;
+  id: number;
+};
 
 @Component({
   selector: 'app-side-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-navbar.component.html',
   styleUrls: ['./side-navbar.component.scss'],
 })
 export class SideNavbarComponent implements OnInit {
+  public categories: Category[] = [
+    {
+      name: 'Categoria 1',
+      totalMessages: 20,
+      id: 1,
+    },
+    {
+      name: 'Categoria 2',
+      totalMessages: 20,
+      id: 2,
+    },
+    {
+      name: 'Categoria 3',
+      totalMessages: 20,
+      id: 3,
+    },
+    {
+      name: 'Categoria 4',
+      totalMessages: 20,
+      id: 4,
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
+
+  public showMessages(category: Category) {
+    console.log(category);
+  }
 }
